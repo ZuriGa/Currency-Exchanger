8a8aa890357c9e1ff9fb1ed3914f2f179bec144b
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-17 23:58:42 -0800

    adding currency exchange code

---
+++ src/js/currencyEx.js
@@ -0,0 +1,16 @@
+// Business Logic   
+export default class CurrencyExchange {
+    static async getCurrency(usd) {
+        try {
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+            const jsonResponse = await response.json();
+            if (!response.ok) {
+                const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
+                throw new Error(errorMessage);
+            }
+            return jsonResponse;
+        } catch(error) {
+            return error;
+        }
+    }
+}
