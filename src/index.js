36fb6f5ba888bfdeb727ca308c8f0632f99c7f9a
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-18 01:03:31 -0800

    will display currency rates base of USD

--- src/index.js
+++ src/index.js
@@ -14,11 +14,43 @@ async function getCurrency(usd) {
 
 // UI Logic
 function printElements(response, usd) {
-  document.querySelector('#displayResults').innerText = `Here is the current rate for ${usd}.`;
   const resultContainer = document.querySelector('#displayResults');
-  response.forEach(function(currency){
-    const listItem = document.createElement("li");
-    listItem.textContent = `Conversion Rate: ${currency.conversion_rates}.`;
-    resultContainer.appendChild(listItem);
-  });
+  resultContainer.innerText = `Here is the current rate for $${usd}.`;
+
+  for(const [currency, rate] of Object.entries(response.conversion_rates)) {
+    const listItems = document.createElement("li");
+    listItems.textContent = `${currency}: ${rate}`;
+    resultContainer.appendChild(listItems);
+  }
 }
+
+function printError(error, usd) {
+    let errorMessage = '';
+    
+    if (!usd || !isValidUsd(usd)) {
+        errorMessage = `Invalid currency: ${usd}. Please enter a valid currency.`;
+    } else {
+        errorMessage = `There was an error accessing your currency exchange for ${usd}: ${error}`
+    }
+    document.querySelector('#displayResults').innerText = errorMessage;
+}
+
+function isValidUsd(usd) {
+    return usd.length >= 0;
+}
+
+function handleFormSubmission(e) {
+    e.preventDefault();
+    const usd = document.querySelector('#currency').value;
+    document.querySelector('#currency').value;
+
+    if(isValidUsd(usd)) {
+        getCurrency(usd);
+    } else {
+        printError('Invalid currency. Please enter a valid amount.', usd);
+    }
+}
+
+window.addEventListener("load", function () {
+    document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
